128337cf94bd5c33b7bda30a7a1dc1252d889880
Johannes Rauch <johannes.rauch@actindo.com>
2023-10-23 21:16:52 +0200

    ui

--- src/ADSR.cpp
+++ src/ADSR.cpp
@@ -29,16 +29,20 @@ struct ADSR : Module {
 
 	enum Stage {
 		STAGE_A = 0,
-		STAGE_D = 1,
+		STAGE_DS = 1,
 		STAGE_R = 2
 	};
 
 	float voltage = 0;
 	int stage = STAGE_R;
 
+	dsp::TSchmittTrigger<float> triggerGate;
+	dsp::TSchmittTrigger<float> triggerRetrigger;
+
 	float attackCoeff = 0.f;
 	float decayCoeff = 0.f;
 	float releaseCoeff = 0.f;
+	float sustainLevel = 0.5f;
 
 	float lastAttackParam = 0.f;
 	float lastDecayParam = 0.f;
@@ -79,28 +83,27 @@ struct ADSR : Module {
 			releaseCoeff = 1.f - exp(-lastReleaseParam*44100/args.sampleRate);
 		}
 
-		float sustain = params[S_PARAM].getValue() + params[SUSMOD_PARAM].getValue() * inputs[SUSMOD_INPUT].getVoltage();
-		sustain = clamp(sustain, 0.f, 10.f);
-
-		float gate = inputs[GATE_INPUT].getVoltage() - inputs[SUSMOD_PARAM].getVoltage();
+		// check release
+		stage = triggerGate.isHigh() ? stage : STAGE_R;
 
 		// check retrig / attack
-		stage = (gate > 1.f && stage == STAGE_R) ? STAGE_A : stage;
-		// check release
-		stage = (gate < 0.1f && stage != STAGE_R) ? STAGE_R : stage;
+		stage = triggerGate.process(inputs[GATE_INPUT].getVoltage(), 0.1f, 1.f) ? STAGE_A : stage;
+		stage = triggerRetrigger.process(inputs[RETRIG_INPUT].getVoltage(), 0.1f, 1.f) ? STAGE_A : stage;
 
 		switch (stage)
 		{
 			case STAGE_A:
-				voltage = attackCoeff*voltage + (1.f-attackCoeff)*gate*1.2f;
+				voltage = attackCoeff*voltage + (1.f-attackCoeff)*12.f;
 				if (voltage > 10.f)
 				{
 					voltage = 10.f;
-					stage = STAGE_D;
+					stage = STAGE_DS;
 				}
 				break;
-			case STAGE_D:
-				voltage = decayCoeff*voltage + (1.f-decayCoeff)*sustain;
+			case STAGE_DS:
+				sustainLevel = params[S_PARAM].getValue() + params[SUSMOD_PARAM].getValue() * inputs[SUSMOD_INPUT].getVoltage();
+				sustainLevel = clamp(sustainLevel, 0.f, 10.f);
+				voltage = decayCoeff*voltage + (1.f-decayCoeff)*sustainLevel;
 				break;
 			case STAGE_R:
 				voltage = releaseCoeff*voltage;
@@ -109,7 +112,7 @@ struct ADSR : Module {
 		// velocity
 		float velScaling = 1.f - params[VELSCALE_PARAM].getValue() + 0.1f*inputs[VEL_INPUT].getVoltage() * params[VELSCALE_PARAM].getValue();
 
-		outputs[SGATE_OUTPUT].setVoltage((stage == STAGE_D) ? 10.f : 0.f);
+		outputs[SGATE_OUTPUT].setVoltage((stage == STAGE_DS) ? 10.f : 0.f);
 		outputs[ENV_OUTPUT].setVoltage(velScaling * voltage);
 	}
 };
@@ -126,10 +129,10 @@ struct ADSRWidget : ModuleWidget {
 		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
 		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(7.62, 16.062)), module, ADSR::A_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(22.86, 24.094)), module, ADSR::VELSCALE_PARAM));
+		addParam(createParamCentered<RoundSmallBlackKnob>(mm2px(Vec(22.86, 24.094)), module, ADSR::VELSCALE_PARAM));
 		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(7.62, 32.125)), module, ADSR::D_PARAM));
 		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(7.62, 48.188)), module, ADSR::S_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(22.86, 56.219)), module, ADSR::SUSMOD_PARAM));
+		addParam(createParamCentered<RoundSmallBlackKnob>(mm2px(Vec(22.86, 56.219)), module, ADSR::SUSMOD_PARAM));
 		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(7.62, 64.25)), module, ADSR::R_PARAM));
 
 		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(7.62, 80.313)), module, ADSR::VEL_INPUT));
