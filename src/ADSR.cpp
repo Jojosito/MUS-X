aeabcc763366d0ccf7e43ac7c16b6fd613b721ad
Johannes Rauch <johannes.rauch@actindo.com>
2023-10-23 20:55:56 +0200

    more features

--- src/ADSR.cpp
+++ src/ADSR.cpp
@@ -4,26 +4,29 @@
 struct ADSR : Module {
 	enum ParamId {
 		A_PARAM,
+		VELSCALE_PARAM,
 		D_PARAM,
 		S_PARAM,
+		SUSMOD_PARAM,
 		R_PARAM,
-		VELSCALE_PARAM,
 		PARAMS_LEN
 	};
 	enum InputId {
+		VEL_INPUT,
+		SUSMOD_INPUT,
 		GATE_INPUT,
 		RETRIG_INPUT,
-		VEL_INPUT,
 		INPUTS_LEN
 	};
 	enum OutputId {
+		SGATE_OUTPUT,
 		ENV_OUTPUT,
 		OUTPUTS_LEN
 	};
 	enum LightId {
-		BLINK_LIGHT,
 		LIGHTS_LEN
 	};
+
 	enum Stage {
 		STAGE_A = 0,
 		STAGE_D = 1,
@@ -43,24 +46,30 @@ struct ADSR : Module {
 
 	ADSR() {
 		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
-		configParam(A_PARAM, 2.f, 15.f, 0.f, "Attack");
-		configParam(D_PARAM, 2.f, 15.f, 0.f, "Decay");
-		configParam(S_PARAM, 0.f, 10.f, 5.f, "Sustain");
-		configParam(R_PARAM, 2.f, 15.f, 0.f, "Release");
-		configParam(VELSCALE_PARAM, 0.f, 1.f, 0.f, "Velocity scaling");
-		configInput(GATE_INPUT, "Gate");
-		configInput(RETRIG_INPUT, "Retrigger");
-		configInput(VEL_INPUT, "Velocity");
-		configOutput(ENV_OUTPUT, "Envelope");
+		configParam(A_PARAM, 2.f, 15.f, 0.f, "Attack time");
+		configParam(D_PARAM, 2.f, 15.f, 0.f, "Decay time");
+		configParam(S_PARAM, 0.f, 10.f, 5.f, "Sustain level");
+		configParam(R_PARAM, 2.f, 15.f, 0.f, "Release time");
+
+		configParam(VELSCALE_PARAM, 0.f, 1.f, 0.f, "Velocity CV");
+		configParam(SUSMOD_PARAM, -1.f, 1.f, 0.f, "Sustain CV");
+
+		configInput(VEL_INPUT, "Velocity CV input");
+		configInput(SUSMOD_INPUT, "Sustain CV input");
+
+		configInput(GATE_INPUT, "Gate input");
+		configInput(RETRIG_INPUT, "Retrigger input");
+
+		configOutput(SGATE_OUTPUT, "Decay/Sustain stage output");
+		configOutput(ENV_OUTPUT, "Envelope output");
 	}
 
 	void process(const ProcessArgs& args) override {
-		// coefficient calculation; only recalculate if params changed
+		// coefficient calculation; only recalculate if params have changed
 		if (params[A_PARAM].getValue() != lastAttackParam ||
 			params[D_PARAM].getValue() != lastDecayParam ||
 			params[R_PARAM].getValue() != lastReleaseParam)
 		{
-			lights[ENV_OUTPUT].setBrightness(1);
 			lastAttackParam = params[A_PARAM].getValue();
 			lastDecayParam = params[D_PARAM].getValue();
 			lastReleaseParam = params[R_PARAM].getValue();
@@ -68,12 +77,12 @@ struct ADSR : Module {
 			attackCoeff = 1.f - exp(-lastAttackParam*44100/args.sampleRate);
 			decayCoeff = 1.f - exp(-lastDecayParam*44100/args.sampleRate);
 			releaseCoeff = 1.f - exp(-lastReleaseParam*44100/args.sampleRate);
-			lights[ENV_OUTPUT].setBrightness(0);
 		}
 
-		float sustainParam = params[S_PARAM].getValue();
+		float sustain = params[S_PARAM].getValue() + params[SUSMOD_PARAM].getValue() * inputs[SUSMOD_INPUT].getVoltage();
+		sustain = clamp(sustain, 0.f, 10.f);
 
-		float gate = inputs[GATE_INPUT].getVoltage() - inputs[RETRIG_INPUT].getVoltage();
+		float gate = inputs[GATE_INPUT].getVoltage() - inputs[SUSMOD_PARAM].getVoltage();
 
 		// check retrig / attack
 		stage = (gate > 1.f && stage == STAGE_R) ? STAGE_A : stage;
@@ -91,7 +100,7 @@ struct ADSR : Module {
 				}
 				break;
 			case STAGE_D:
-				voltage = decayCoeff*voltage + (1.f-decayCoeff)*sustainParam;
+				voltage = decayCoeff*voltage + (1.f-decayCoeff)*sustain;
 				break;
 			case STAGE_R:
 				voltage = releaseCoeff*voltage;
@@ -99,6 +108,8 @@ struct ADSR : Module {
 
 		// velocity
 		float velScaling = 1.f - params[VELSCALE_PARAM].getValue() + 0.1f*inputs[VEL_INPUT].getVoltage() * params[VELSCALE_PARAM].getValue();
+
+		outputs[SGATE_OUTPUT].setVoltage((stage == STAGE_D) ? 10.f : 0.f);
 		outputs[ENV_OUTPUT].setVoltage(velScaling * voltage);
 	}
 };
@@ -115,18 +126,19 @@ struct ADSRWidget : ModuleWidget {
 		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
 		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(7.62, 16.062)), module, ADSR::A_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(22.86, 24.094)), module, ADSR::VELSCALE_PARAM));
 		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(7.62, 32.125)), module, ADSR::D_PARAM));
 		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(7.62, 48.188)), module, ADSR::S_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(22.86, 56.219)), module, ADSR::SUSMOD_PARAM));
 		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(7.62, 64.25)), module, ADSR::R_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(7.62, 80.313)), module, ADSR::VELSCALE_PARAM));
 
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(7.62, 80.313)), module, ADSR::VEL_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(22.86, 80.313)), module, ADSR::SUSMOD_INPUT));
 		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(7.62, 96.375)), module, ADSR::GATE_INPUT));
 		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(22.86, 96.375)), module, ADSR::RETRIG_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(7.62, 112.438)), module, ADSR::VEL_INPUT));
 
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(7.62, 112.438)), module, ADSR::SGATE_OUTPUT));
 		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(22.86, 112.438)), module, ADSR::ENV_OUTPUT));
-
-		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(22.86, 80.313)), module, ADSR::BLINK_LIGHT));
 	}
 };
 
