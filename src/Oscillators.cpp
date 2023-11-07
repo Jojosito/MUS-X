83eae7831e3216896ed8877a84904d543362749f
Johannes Rauch <johannes.rauch@actindo.com>
2023-11-07 19:18:30 +0100

    more FM

--- src/Oscillators.cpp
+++ src/Oscillators.cpp
@@ -67,6 +67,8 @@ struct Oscillators : Module {
 	float_4 fm[4] = {0};
 	float_4 ringmod[4] = {0};
 
+	float_4 doSync[4] = {0};
+
 	Oscillators() {
 		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
 		configParam(OSC1SHAPE_PARAM, 	0.f, 1.f, 0.f, 	"Oscillator 1 shape");
@@ -100,6 +102,7 @@ struct Oscillators : Module {
 		for (int c = 0; c < 16; c += 4) {
 			phasor1[c/4] = 0.f;
 			phasor2[c/4] = 0.f;
+			doSync[c/4] = 0.f;
 
 			for (int i = 0; i < maxOversamplingRate; ++i)
 			{
@@ -127,7 +130,7 @@ struct Oscillators : Module {
 			osc2Vol[c/4] *= 5.f / INT32_MAX;
 
 			fm[c/4] 	= simd::clamp(params[FM_INPUT].getValue()  + 0.1f *inputs[FM_INPUT].getVoltageSimd<float_4>(c),  0.f, 1.f);
-			fm[c/4]   = fm[c/4] * fm[c/4] * 0.1f / oversamplingRate; // scale
+			fm[c/4]   = fm[c/4] * fm[c/4] * 0.5f / oversamplingRate; // scale
 			ringmod[c/4] 	= simd::clamp(params[RINGMOD_PARAM].getValue()   + 0.1f *inputs[RINGMOD_INPUT].getVoltageSimd<float_4>(c),   0.f, 1.f);
 			ringmod[c/4] *= 5.f / INT32_MAX / INT32_MAX;
 
@@ -148,13 +151,13 @@ struct Oscillators : Module {
 				case -1: // sync osc 2 to osc 1
 					for (int i = 0; i < oversamplingRate; ++i)
 					{
-						float_4 doSync = phasor1[c/4] + phase1Inc < phasor1[c/4];
+						doSync[c/4] = phasor1[c/4] + phase1Inc < phasor1[c/4];
 
 						phasor1[c/4] += phase1Inc;
 						float_4 wave1 = (phasor1[c/4] - phase1Offset - phase1Offset) * osc1Shape[c/4] - 1.f * phasor1[c/4]; // +-INT32_MAX
 
 						int32_4 phase2IncWithFm = phase2Inc + int32_4(fm[c/4] * wave1);
-						phasor2[c/4] = simd::ifelse(doSync, -INT32_MAX, phasor2[c/4] + phase2IncWithFm);
+						phasor2[c/4] = simd::ifelse(doSync[c/4], -INT32_MAX, phasor2[c/4] + phase2IncWithFm);
 						float_4 wave2 = (phasor2[c/4] - phase2Offset - phase2Offset) * osc2Shape[c/4] - 1.f * phasor2[c/4]; // +-INT32_MAX
 
 						mix[c/4][i] = osc1Vol[c/4] * wave1 + osc2Vol[c/4] * wave2 + ringmod[c/4] * wave1 * wave2; // +-5V each
@@ -163,12 +166,19 @@ struct Oscillators : Module {
 				case 1: // sync osc 1 to osc 2
 					for (int i = 0; i < oversamplingRate; ++i)
 					{
-						float_4 doSync = phasor2[c/4] + phase2Inc < phasor2[c/4];
+						// this is actually wrong, since phasor2 will be incremented with phase2IncWithFm,
+						// but this gives awesome distorted sounds,
+						// whereas the correct implementation gives much more clean but boring sounds
+						doSync[c/4] = phasor2[c/4] + phase2Inc < phasor2[c/4];
 
-						phasor1[c/4] = simd::ifelse(doSync, -INT32_MAX, phasor1[c/4] + phase1Inc);
+						phasor1[c/4] = simd::ifelse(doSync[c/4], -INT32_MAX, phasor1[c/4] + phase1Inc);
 						float_4 wave1 = (phasor1[c/4] - phase1Offset - phase1Offset) * osc1Shape[c/4] - 1.f * phasor1[c/4]; // +-INT32_MAX
 
 						int32_4 phase2IncWithFm = phase2Inc + int32_4(fm[c/4] * wave1);
+
+						// this is more correct but boring
+						//doSync[c/4] = (phase2IncWithFm > 0) & (phasor2[c/4] + phase2IncWithFm < phasor2[c/4]);
+
 						phasor2[c/4] += phase2IncWithFm;
 						float_4 wave2 = (phasor2[c/4] - phase2Offset - phase2Offset) * osc2Shape[c/4] - 1.f * phasor2[c/4]; // +-INT32_MAX
 
