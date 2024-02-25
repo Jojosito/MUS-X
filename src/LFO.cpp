eeb00dc62dd823bd3e6b3c959476628f35ee4d88
Johannes Rauch <johannes.rauch@actindo.com>
2024-02-25 20:29:25 +0100

    WIP

--- src/LFO.cpp
+++ src/LFO.cpp
@@ -4,6 +4,7 @@ namespace musx {
 
 using namespace rack;
 using simd::float_4;
+using simd::int32_4;
 
 struct LFO : Module {
 	enum ParamId {
@@ -27,19 +28,71 @@ struct LFO : Module {
 		LIGHTS_LEN
 	};
 
+	static constexpr float minFreq = 0.004f; // Hz
+	static constexpr float maxFreq = 1000.f; // Hz
+
+	int channels = 1;
+
+	int sampleRateReduction = 1;
+	bool bipolar = true;
+
+	// integers overflow, so phase resets automatically
+	int32_4 phasor[4] = {0};
+
+	dsp::ClockDivider divider;
+
 	LFO() {
 		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
-		configParam(SHAPE_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(FREQ_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(AMP_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(RESET_PARAM, 0.f, 1.f, 0.f, "");
-		configInput(FREQ_INPUT, "");
-		configInput(AMP_INPUT, "");
-		configInput(RESET_INPUT, "");
-		configOutput(OUT_OUTPUT, "");
+		configParam(SHAPE_PARAM, 0.f, 1.f, 0.f, "Shape");
+		configParam(FREQ_PARAM, 0.f, 1.f, 0.5f, "Frequency", " Hz", maxFreq/minFreq, minFreq);
+		configParam(AMP_PARAM, 0.f, 5.f, 0.f, "Amplitude", " V");
+		configButton(RESET_PARAM, "Reset phase");
+		configInput(FREQ_INPUT, "Frequency CV");
+		configInput(AMP_INPUT, "Amplitude CV");
+		configInput(RESET_INPUT, "Reset trigger");
+		configOutput(OUT_OUTPUT, "LFO");
+	}
+
+	void setSampleRateReduction(int arg)
+	{
+		sampleRateReduction = arg;
+		divider.setDivision(sampleRateReduction);
 	}
 
 	void process(const ProcessArgs& args) override {
+		if (divider.process())
+		{
+			//
+			// channels
+			//
+			channels = 1;
+
+			channels = std::max(channels, inputs[FREQ_INPUT].getChannels());
+			channels = std::max(channels, inputs[AMP_INPUT].getChannels());
+			channels = std::max(channels, inputs[RESET_INPUT].getChannels());
+
+			outputs[OUT_OUTPUT].setChannels(channels);
+		}
+	}
+
+	json_t* dataToJson() override {
+		json_t* rootJ = json_object();
+		json_object_set_new(rootJ, "sampleRateReduction", json_integer(sampleRateReduction));
+		json_object_set_new(rootJ, "bipolar", json_boolean(bipolar));
+		return rootJ;
+	}
+
+	void dataFromJson(json_t* rootJ) override {
+		json_t* sampleRateReductionJ = json_object_get(rootJ, "sampleRateReduction");
+		if (sampleRateReductionJ)
+		{
+			setSampleRateReduction(json_integer_value(sampleRateReductionJ));
+		}
+		json_t* bipolarJ = json_object_get(rootJ, "bipolar");
+		if (bipolarJ)
+		{
+			bipolar = json_boolean_value(bipolarJ);
+		}
 	}
 };
 
@@ -64,6 +117,30 @@ struct LFOWidget : ModuleWidget {
 		addInput(createInputCentered<ThemedPJ301MPort>(mm2px(Vec(7.62, 92.671)), module, LFO::RESET_INPUT));
 
 		addOutput(createOutputCentered<ThemedPJ301MPort>(mm2px(Vec(7.62, 112.438)), module, LFO::OUT_OUTPUT));
+	}
+
+	void appendContextMenu(Menu* menu) override {
+		LFO* module = getModule<LFO>();
+
+		menu->addChild(new MenuSeparator);
+
+		menu->addChild(createIndexSubmenuItem("Reduce internal sample rate", {"1x", "2x", "4x", "8x", "16x", "32x", "64x", "128x", "256x", "512x", "1024x"},
+			[=]() {
+				return log2(module->sampleRateReduction);
+			},
+			[=](int mode) {
+				module->setSampleRateReduction(std::pow(2, mode));
+			}
+		));
+
+		menu->addChild(createBoolMenuItem("Bipolar", "",
+			[=]() {
+				return module->bipolar;
+			},
+			[=](int mode) {
+				module->bipolar = mode;
+			}
+		));
 	}
 };
 
