7313f37b715f87c25df71a583828e86071d5b3f8
Johannes Rauch <johannes.rauch@actindo.com>
2024-05-26 15:46:04 +0200

    WIP

--- src/components/componentLibrary.hpp
+++ src/components/componentLibrary.hpp
@@ -6,44 +6,50 @@ namespace musx {
 
 using namespace rack;
 
+struct BipolarColorParamQuantity : ParamQuantity
+{
+	NVGcolor color = nvgRGB(0, 255, 0);
+	bool bipolar = false;
+	bool disabled = false;
+
+};
+
 class RoundBlackKnobWithArc : public RoundBlackKnob {
 private:
 	static constexpr float arcThickness = 1.75f;
 	static constexpr float TOP_ANGLE = 3.0f / 2.0f * M_PI;
 
-	NVGcolor arcColor = nvgRGB(0, 255, 0);
-	bool bipolar = false;
-
 public:
-	void setBipolar(bool b)
-	{
-		bipolar = b;
-	}
-
-	void setArcColor(NVGcolor color)
-	{
-		arcColor = color;
-	}
 
 	void draw(const DrawArgs& args) override
 	{
 		RoundBlackKnob::draw(args);
 
-		auto paramQuantity = getParamQuantity();
+		BipolarColorParamQuantity* paramQuantity = dynamic_cast<BipolarColorParamQuantity*>(getParamQuantity());
+
+		if (!paramQuantity)
+		{
+			return;
+		}
+
+		if (paramQuantity->disabled)
+		{
+			return;
+		}
 
 		float a0 = TOP_ANGLE;
-		if (!bipolar)
+		if (!paramQuantity->bipolar)
 		{
 			a0 += minAngle;
 		}
 		float a1 = TOP_ANGLE + math::rescale(paramQuantity->getValue(), paramQuantity->getMinValue(), paramQuantity->getMaxValue(), minAngle, maxAngle);
 
-		drawArc(args, a0, a1);
+		drawArc(args, a0, a1, paramQuantity->color);
 	}
 
 
 private:
-	void drawArc(const DrawArgs &args, float a0, float a1) {
+	void drawArc(const DrawArgs &args, float a0, float a1, NVGcolor arcColor) {
 
 		int dir = a1 > a0 ? NVG_CW : NVG_CCW;
 		Vec cVec = box.size.div(2.0f);
@@ -57,7 +63,7 @@ private:
 		nvgStrokeColor(args.vg, nvgRGB(32, 32, 32));
 		nvgStroke(args.vg);
 
-		// color
+		// color // TODO must be a light!
 		nvgBeginPath(args.vg);
 		nvgLineCap(args.vg, NVG_ROUND);
 		nvgArc(args.vg, cVec.x, cVec.y, r, a0, a1, dir);
